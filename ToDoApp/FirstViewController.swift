c7a8936b47a6259637b8d117974efd2eb6eb4990
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-08 10:00:15 +0530

    Todo App COmplete

--- ToDoApp/FirstViewController.swift
+++ ToDoApp/FirstViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    var toDoArray = [String] ()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if let todosObjectArray = UserDefaults.standard.object(forKey: "todoArray") as? [String]
+        {
+            toDoArray = todosObjectArray
+            table.reloadData()
+        }
+        
+        
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return toDoArray.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
     }
 
 
--- ToDoApp/SecondViewController.swift
+++ ToDoApp/SecondViewController.swift
@@ -8,19 +8,41 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func addToDo(_ sender: Any) {
+        if let toDo = self.textField.text {
+            toDoArray.append(toDo)
+            UserDefaults.standard.set(self.toDoArray, forKey: "todoArray")
+            textField.text = ""
+        }
     }
+    var toDoArray = [String]()
+    
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let todosObjectArray = UserDefaults.standard.object(forKey: "todoArray") as? [String]
+        {
+                self.toDoArray = todosObjectArray
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
